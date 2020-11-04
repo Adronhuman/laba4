8b4f6add42605f199ccc2e2902441dd987011eae
Andrii Riabchuk <andrii.riabchuk@devcom.com>
2020-11-04 11:12:04 +0200

    finished lab

--- solve.py
+++ solve.py
@@ -1,35 +1,9 @@
-#      a1  a2 a3
-from decimal import Decimal
-import decimal
-p = [0, 0.042, 0.083, 0.125, 0.042, 0.042, 0.042, 0.042, 0.042, 0.125, 0.042, 0.083, 0.042, 0.042, 0.042, 0.042, 0.083, 0.042]
-p = [decimal.Decimal(i) for i in p]
-print(len(p))
-s =  "10 4 2  5"
+from flask import Flask
+app = Flask(__name__)
 
-popka = [[]]
+@app.route("/api/v1/hello-world/22")
+def hello():
+  return "Hello World!"
 
-buf = 0
-
-for i in range(len(p)-1):
-  c = p[i+1]
-  popka.append([buf,buf+c])
-  buf += c
-print(popka)
-print(len(s.split()))
-r = [0,1]
-c = 0
-suka = []
-for i in [int(i) for i in s.split()]:
-  suka.append(r[::])
-  print(c,r)
-  d = r[1]-r[0]
-  print(i)
-  r[1] = round(r[0]+popka[i][1]*d,20)
-  r[0] = round(r[0]+popka[i][0]*d,20)
-  c+=1
-print(c,r)
-for i in range(4):
-  print(suka[i][0])
-print()
-for i in range(4):
-  print(suka[i][1])
+if __name__ == "__main__":
+  app.run()
